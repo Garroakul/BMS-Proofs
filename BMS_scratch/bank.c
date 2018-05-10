8f6c7cc4cd39349fb9e85f06d0b68cf628621927
Yilun <liyiluan@udel.edu>
2018-05-10 15:46:52 -0400

    syntax fix- check line changes

--- BMS_scratch/bank.c
+++ BMS_scratch/bank.c
@@ -1,231 +1,231 @@
 class BankAccount {
 
-  /* Simple container class.
-     Holds owner name, balance, and an arbitrary ID.
-     Data is currently open, can also be set private as needed.
-   */
-  
-  string owner;
-  int acctID;
-  double balance;
+    /* Simple container class.
+       Holds owner name, balance, and an arbitrary ID.
+       Data is currently open, can also be set private as needed.
+     */
 
-  BankAccount(string opener, int thisID, double startBalance) {
-    owner = opener;
-    acctID = thisID;
-    balance = startBalance;
-  }
-}
+    string owner;
+    int acctID;
+    double balance;
+
+    BankAccount(string opener, int thisID, double startBalance) {
+        owner = opener;
+        acctID = thisID;
+        balance = startBalance;
+    }
+};
 
 class BigBank {
 
-  /* Overall managing class for the bank accounts.
-     Max sets the maximum accounts for alloc purposes.
-     Total should be lock-stepped with accountList's allocation.
-     List is the collection of accounts in the bank.
-   */
+    /* Overall managing class for the bank accounts.
+       Max sets the maximum accounts for alloc purposes.
+       Total should be lock-stepped with accountList's allocation.
+       List is the collection of accounts in the bank.
+     */
 
-  BankAccount[] accountList;
-  int accountMax;
-  int accountTotal;
+    //BankAccount[] accountList;
+    BankAccount acccountList[100];
 
-  // Standard assignment constructor.
-  
-  BigBank(int max) {
-    accountMax = max;
-    accountList = new BankAccount[max];
-    accountTotal = 0;
-  }
+    int accountMax;
+    int accountTotal;
 
-  /* Next two methods are account grabbers.
-     Feed either an ID or an owner string and it'll return the account.
-     Or null if there's no account.
-   */
-  
-  BankAccount accountByOwner (string input) {
-    for(int i=0;i<accountTotal;i++) {
-      BankAccount thisAcct = accountList[i];
-      if (thisAcct.owner==input) return thisAcct;
+    // Standard assignment constructor.
+
+    BigBank(int max) {
+        accountMax = max;
+        accountList = new BankAccount[max];
+        accountTotal = 0;
     }
-    return null;
-  }
 
-  BankAccount accountByID (int ID) {
-    if (ID > accountTotal) return null;
-    else return accountList[ID-1];
-  }
+    /* Next two methods are account grabbers.
+       Feed either an ID or an owner string and it'll return the account.
+       Or null if there's no account.
+     */
 
-  // Index from account is also handy.
-
-  int indexFromAccount (BankAccount thisAccount) {
-    for(int i=0;i<accountTotal;i++) {
-      if (accountList[i] == thisAccount) return i;
+    BankAccount accountByOwner(string input) {
+        for (int i = 0; i < accountTotal; i++) {
+            BankAccount thisAcct = accountList[i];
+            if (thisAcct.owner == input) return thisAcct;
+        }
+        return null;
     }
-    return null;
-  }
-  
-  // Next up is opening and closing accounts.
 
-  bool createAccount (string person, double startBal) {
-    accountList[accountTotal] = new BankAccount(person, accountTotal+1, startBal);
-    accountTotal++;
-    return true;
-  }
-
-  bool deleteAccountOwner (string owner) {
-    BankAccount toDelete = accountByOwner(owner);
-    if (toDelete != null) {
-      int indDel = indexFromAccount(toDelete);
-      free(toDelete);
-      accountList[indDel] = null;
-      return true;
+    BankAccount accountByID(int ID) {
+        if (ID > accountTotal) return null;
+        else return accountList[ID - 1];
     }
-    else return false;
-  }
 
-  bool deleteAccountID (int ID) {
-    BankAccount toDelete = accountByID(ID);
-    if (toDelete != null) {
-      int indDel = indexFromAccount(toDelete);
-      free(toDelete);
-      accountList[indDel] = null;
-      return true;
+    // Index from account is also handy.
+
+    int indexFromAccount(BankAccount thisAccount) {
+        for (int i = 0; i < accountTotal; i++) {
+            if (accountList[i] == thisAccount) return i;
+        }
+        return null;
     }
-    else return false;
-  }
 
-  // Input owner name, get ID out. Easy.
-  
-  int findIDByOwner (string owner) {
-    BankAccount thisAcct = accountByOwner(owner);
-    return thisAcct.acctID;
-  }
+    // Next up is opening and closing accounts.
 
-  // Deposits and Withdrawals. Withdraw has a bool check for sufficient funds.
-  
-  void makeDeposit (BankAccount thisAcct, double amt) {
-    thisAcct.balance += amt;
-  }
-
-  void makeDeposit (string owner, double amt) {
-    BankAccount thisAcct = accountByOwner(owner);
-    makeDeposit(thisAcct, amt);
-  }
-
-  void makeDeposit (int ID, double amt) {
-    BankAccount thisAcct = accountByID(ID);
-    makeDeposit(thisAcct, amt);
-  }
-  
-  bool makeWithdraw (BankAccount thisAcct, double amt) {
-    if(thisAcct.balance < amt) return false;
-    thisAcct.balance -= amt;
-    return true;
-  }
-
-  bool makeWithdraw (string owner, double amt) {
-    BankAccount thisAcct = accountByOwner(owner);
-    return makeWithdraw(thisAcct, amt);
-  }
-
-  bool makeWithdraw (int ID, double amt) {
-    BankAccount thisAcct = accountByID(ID);
-    return makeWithdraw(thisAcct, amt);
-  }
-
-  // Transfer implemented as paired withdrawal/deposit.
-  
-  bool makeTransfer (BankAccount accFrom, BankAccount accTo, double amt) {
-    bool active = true;
-    active = makeWithdraw(accFrom, amt);
-    if (!active) return active;
-    makeDeposit(accTo, amt);
-    return active;
-  }
-
-  bool makeTransfer (string ownerFrom, string ownerTo, double amt) {
-    BankAccount accFrom = accountByOwner(ownerFrom);
-    BankAccount accTo = accountByOwner(ownerTo);
-    return makeTransfer (accFrom, accTo, amt);
-  }
-
-  bool makeTransfer (int IDFrom, int IDTo, double amt) {
-    BankAccount accFrom = accountByID(IDFrom);
-    BankAccount accTo = accountByID(IDTo);
-    return makeTransfer (accFrom, accTo, amt);
-  }
-
-  void addInterest (BankAccount thisAcct, double rate, double years) {
-    if (rate<0) return;
-    thisAcct.balance *= ((1+rate)*years);
-  }
-
-  void addInterest (string owner, double rate, double years) {
-    BankAccount thisAcct = accountByOwner(owner);
-    return addInterest (thisAcct, rate, years);
-  }
-
-  void addInterest (int ID, double rate, double years) {
-    BankAccount thisAcct = accountByID(ID);
-    return addInterest (thisAcct, rate, years);
-  }
-  
-  // Getters for total account number and total balance.
-
-  int accountNumber () {
-    int accCount = 0;
-    for (int i=0;i<accountTotal;i++) {
-      if (accountList[i]!=null) accCount++;
+    bool createAccount(string person, double startBal) {
+        accountList[accountTotal] = new BankAccount(person, accountTotal + 1, startBal);
+        accountTotal++;
+        return true;
     }
-    return accCount;
-  }
 
-  double totalMoney () {
-    double money = 0;
-    for (int i=0;i<accountTotal;i++) {
-      if (accountList[i]!=null) money += accountList[i].balance;
+    bool deleteAccountOwner(string owner) {
+        BankAccount toDelete = accountByOwner(owner);
+        if (toDelete != null) {
+            int indDel = indexFromAccount(toDelete);
+            free(toDelete);
+            accountList[indDel] = null;
+            return true;
+        } else return false;
     }
-    return money;
-  }
 
-  // Mainenance functions.
+    bool deleteAccountID(int ID) {
+        BankAccount toDelete = accountByID(ID);
+        if (toDelete != null) {
+            int indDel = indexFromAccount(toDelete);
+            free(toDelete);
+            accountList[indDel] = null;
+            return true;
+        } else return false;
+    }
 
-  bool expandSize (int newSize) {
-    if (newSize < accountMax) return false;
-    else {
-      accountMax = newSize;
-      return true;
-    }
-  }
+    // Input owner name, get ID out. Easy.
 
-  /* This warrants some explanation.
-     This function fixes the annoying issue with arrays -
-     - that as you add/remove entries it looks more and more like Swiss Cheese.
-     The account list is copied over to a list.
-     It is then consolidated as it is copied over to the old list.
-   */
-  
-  void resizeSet () {
-    BigBank tempList = BankAccount[accountMax];
-    for(int i=0;i<accountTotal;i++) {
-      tempList[i] = accountList[i];
-      accountList[i] = null;
+    int findIDByOwner(string owner) {
+        BankAccount thisAcct = accountByOwner(owner);
+        return thisAcct.acctID;
     }
-    int slot = 0;
-    for(int i=0;i<accountTotal;i++) {
-      if(tempList[i]!=null) {
-	accountList[slot] = tempList[i];
-	slot++;
-      }
+
+    // Deposits and Withdrawals. Withdraw has a bool check for sufficient funds.
+
+    void makeDeposit(BankAccount thisAcct, double amt) {
+        thisAcct.balance += amt;
     }
-    int newCount = 0;
-    for(int i=0;i<accountTotal;i++) {
-      if(accountList[i]!=null) newCount++;
+
+    void makeDeposit(string owner, double amt) {
+        BankAccount thisAcct = accountByOwner(owner);
+        makeDeposit(thisAcct, amt);
     }
-    accountTotal = newCount;
-  }
-}
+
+    void makeDeposit(int ID, double amt) {
+        BankAccount thisAcct = accountByID(ID);
+        makeDeposit(thisAcct, amt);
+    }
+
+    bool makeWithdraw(BankAccount thisAcct, double amt) {
+        if (thisAcct.balance < amt) return false;
+        thisAcct.balance -= amt;
+        return true;
+    }
+
+    bool makeWithdraw(string owner, double amt) {
+        BankAccount thisAcct = accountByOwner(owner);
+        return makeWithdraw(thisAcct, amt);
+    }
+
+    bool makeWithdraw(int ID, double amt) {
+        BankAccount thisAcct = accountByID(ID);
+        return makeWithdraw(thisAcct, amt);
+    }
+
+    // Transfer implemented as paired withdrawal/deposit.
+
+    bool makeTransfer(BankAccount accFrom, BankAccount accTo, double amt) {
+        bool active = true;
+        active = makeWithdraw(accFrom, amt);
+        if (!active) return active;
+        makeDeposit(accTo, amt);
+        return active;
+    }
+
+    bool makeTransfer(string ownerFrom, string ownerTo, double amt) {
+        BankAccount accFrom = accountByOwner(ownerFrom);
+        BankAccount accTo = accountByOwner(ownerTo);
+        return makeTransfer(accFrom, accTo, amt);
+    }
+
+    bool makeTransfer(int IDFrom, int IDTo, double amt) {
+        BankAccount accFrom = accountByID(IDFrom);
+        BankAccount accTo = accountByID(IDTo);
+        return makeTransfer(accFrom, accTo, amt);
+    }
+
+    void addInterest(BankAccount thisAcct, double rate, double years) {
+        if (rate < 0) return;
+        thisAcct.balance *= ((1 + rate) * years);
+    }
+
+    void addInterest(string owner, double rate, double years) {
+        BankAccount thisAcct = accountByOwner(owner);
+        return addInterest(thisAcct, rate, years);
+    }
+
+    void addInterest(int ID, double rate, double years) {
+        BankAccount thisAcct = accountByID(ID);
+        return addInterest(thisAcct, rate, years);
+    }
+
+    // Getters for total account number and total balance.
+
+    int accountNumber() {
+        int accCount = 0;
+        for (int i = 0; i < accountTotal; i++) {
+            if (accountList[i] != null) accCount++;
+        }
+        return accCount;
+    }
+
+    double totalMoney() {
+        double money = 0;
+        for (int i = 0; i < accountTotal; i++) {
+            if (accountList[i] != null) money += accountList[i].balance;
+        }
+        return money;
+    }
+
+    // Mainenance functions.
+
+    bool expandSize(int newSize) {
+        if (newSize < accountMax) return false;
+        else {
+            accountMax = newSize;
+            return true;
+        }
+    }
+
+    /* This warrants some explanation.
+       This function fixes the annoying issue with arrays -
+       - that as you add/remove entries it looks more and more like Swiss Cheese.
+       The account list is copied over to a list.
+       It is then consolidated as it is copied over to the old list.
+     */
+
+    void resizeSet() {
+        BigBank tempList = BankAccount[accountMax];
+        for (int i = 0; i < accountTotal; i++) {
+            tempList[i] = accountList[i];
+            accountList[i] = null;
+        }
+        int slot = 0;
+        for (int i = 0; i < accountTotal; i++) {
+            if (tempList[i] != null) {
+                accountList[slot] = tempList[i];
+                slot++;
+            }
+        }
+        int newCount = 0;
+        for (int i = 0; i < accountTotal; i++) {
+            if (accountList[i] != null) newCount++;
+        }
+        accountTotal = newCount;
+    }
+};
 
 int main() {
-  BigBank thisBank = new BigBank(100);
-  
+    BigBank thisBank = new BigBank(100);
+
 }
